b90928fa03d7a9aecf89c11e0566fdd665c04537
doron zarhi <doron88@gmail.com>
2022-11-27 15:10:29 +0200

    setup: add python 3.11 classifier

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
             'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
         ],
         url='https://github.com/matan1008/pygnuutils',
         project_urls={
