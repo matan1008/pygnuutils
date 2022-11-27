39b7c9a94a0e1cd55c121f91f67dabccf9ff6f9f
matan <matan1008@gmail.com>
2022-11-27 15:38:02 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 BASE_DIR = Path(__file__).parent.resolve(strict=True)
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 PACKAGE_NAME = 'pygnuutils'
 PACKAGES = [p for p in find_packages() if not p.startswith('tests')]
 
